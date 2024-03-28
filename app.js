37a51223bb76edba63e3471a297bc3380b4df5b4
anandsathy <152793614+anandsathy@users.noreply.github.com>
2024-03-28 16:33:09 +0000

    asdasda

--- app.js
+++ app.js
@@ -6,6 +6,28 @@ app.get('/', (req, res) => {
   res.status(418).send("sathy")
 })
 
+const users = [
+  {username: "sathy", password: "sathy"}
+];
+
+app.post('/cp', function(req, res, next) {
+  const { username, password } = req.body;
+  const user = users.find(user => user.username === username && user.password === password);
+
+  if (user){
+    res.status(200).json({ success: true, username: user.username });
+    console.log('apporved user ' + user.username + "they had enough sathyness to pass" );
+
+
+  } else {
+    res.status(401).json({ success: false, message: "no sathyness to pass!!" });
+    console.log('denied user ' + username + "they had enough sathyness to pass" );
+  };
+
+
+
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
