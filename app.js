e568bb42b247f52154252bbe472d09c02191336e
anandsathy <152793614+anandsathy@users.noreply.github.com>
2024-03-28 16:19:42 +0000

    sathy and kim dev

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.status(418).send("sathy")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
